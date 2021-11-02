7c59de4043cf4ae7015bdfac6939c2a9b85f5903
wwww.anechka@gmail.com <wwww.anechka@gmail.com>
2021-11-02 18:27:16 +0300

    Moved js code for comparing login and password in database to new js file

---
+++ WebTestCore/wwwroot/js/checkInputParameters.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $(document).on('click', '#sbm', function() {
+
+        $.ajax({
+            method: "POST",
+            url: "/Security/Ajax",
+            data: { "login": document.getElementById('login').value },
+            dataType: "json",
+
+            success: function (data) {
+                let serialize = $.parseJSON(data);
+
+                if (serialize == null) {
+                    $('form').submit();
+                    return;
+                }
+
+                $(".login").append(serialize["Login"]);
+                $(".password").append(serialize["Password"]);
+                const elemModal = document.querySelector('.modal');
+                const modal = new bootstrap.Modal(elemModal, {
+                    backdrop: true,
+                    keyboard: true,
+                    focus: true,
+                });
+                modal.show();
+            },
+
+            error: function () {
+                alert("Failed.");
+            },
+        })
+    });
+});
